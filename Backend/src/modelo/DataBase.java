d91df29d30acad84b1ab7ada08758441dc2c7b00
David Arias <davidariascalderon04@gmail.com>
2024-04-18 11:04:42 -0500

    agregacion de funciones basicas de consulta en la base de datos, hace falta discutir el diseño o el patron de diseño para la conexion a la base de datos, se puede mejorar la conexion y busquedas

--- Backend/src/modelo/DataBase.java
+++ Backend/src/modelo/DataBase.java
@@ -26,24 +26,126 @@ public class DataBase {
         }
     }    
 
-    public void crearCliente(String nombre,String identificacion,String contrasena,String correo,String usuario){
+    public boolean crearUsuario(String nombre,String identificacion,String contrasena,String correo,String usuario){
         conectar();
         try{
             String sqlInsercion = "INSERT INTO usuario (Nombre, Identificacion, Contrasena, Correo, Usuario) VALUES (?, ?, ?, ?, ?)";
-            try (
-                PreparedStatement ps = conexion.prepareStatement(sqlInsercion)) {
-                ps.setString(1, nombre);
-                ps.setString(2, identificacion);
-                ps.setString(3, contrasena);
-                ps.setString(4, correo);
-                ps.setString(5, usuario);
-                ps.executeUpdate();
-            }
+            PreparedStatement ps = conexion.prepareStatement(sqlInsercion);
+            ps.setString(1, nombre);
+            ps.setString(2, identificacion);
+            ps.setString(3, contrasena);
+            ps.setString(4, correo);
+            ps.setString(5, usuario);
+            ps.executeUpdate();
+            return true;
         }catch(Exception e){
-            System.out.println("Error: "+e);
+            return false;
         }
     }
- 
+
+    public ResultSet buscarIdUsuario(String usuario){
+        conectar();
+        try{
+            String sqlQuery = "SELECT Id FROM USUARIO WHERE usuario = ?";
+            PreparedStatement ps = conexion.prepareStatement(sqlQuery);
+            ps.setString(1, usuario);
+            System.out.println(ps.toString());
+            rs = ps.executeQuery();
+            if(tieneResultados(rs)){
+                return rs;
+            }else{
+                return null;
+            }
+        }catch(SQLException ex){
+            return null;
+        }
+    }
+
+    public ResultSet buscarUsuario(String contrasena, String usuario){
+        conectar();
+        try{
+            String sqlQuery = "SELECT ESTADO, CORREO FROM USUARIO WHERE contrasena = ? and usuario = ?";
+            PreparedStatement ps = conexion.prepareStatement(sqlQuery);
+            ps.setString(1, contrasena);
+            ps.setString(2, usuario);
+            rs = ps.executeQuery();
+            if(tieneResultados(rs)){
+                return rs;
+            }else{
+                return null;
+            }
+        }catch(SQLException ex){
+            return null;
+        }
+    }
+    
+    public boolean crearIpConUsuario(String direccionIp,int usuarioFk){
+        conectar();
+        try{
+            String sqlInsercion = "INSERT INTO ip (Direccion_ip, Usuario_fk) VALUES (?, ?)";
+            PreparedStatement ps = conexion.prepareStatement(sqlInsercion);
+            ps.setString(1, direccionIp);
+            ps.setInt(2, usuarioFk);
+            ps.executeUpdate();
+            return true;
+        }catch(Exception e){
+            System.out.println("Error: "+e);
+            return false;
+        }
+    }
+
+    public boolean crearIpSinUsuario(String direccionIp){
+        conectar();
+        try{
+            String sqlInsercion = "INSERT INTO ip (Direccion_ip) VALUES (?)";
+            PreparedStatement ps = conexion.prepareStatement(sqlInsercion);
+            ps.setString(1, direccionIp);
+            ps.executeUpdate();
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public boolean tieneResultados(ResultSet rs) {
+        try {
+            return rs.isBeforeFirst();
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public boolean crearTipoUsuario(String tipo,int usuarioFk){
+        conectar();
+        try{
+            String sqlInsercion = "INSERT INTO tipo_usuario (Tipo, Usuario_fk) VALUES (?, ?)";
+            PreparedStatement ps = conexion.prepareStatement(sqlInsercion);
+            ps.setString(1, tipo);
+            ps.setInt(2, usuarioFk);
+            ps.executeUpdate();
+            return true;
+        }catch(Exception e){
+            System.out.println("Error: "+e);
+            return false;
+        }
+    }
+
+    public ResultSet buscarTipoUsuario(int usuarioFk){
+        conectar();
+        try{
+            String sqlInsercion = "SELECT * FROM tipo_usuario WHERE Usuario_fk = ?";
+            PreparedStatement ps = conexion.prepareStatement(sqlInsercion);
+            ps.setInt(1, usuarioFk);
+            rs = ps.executeQuery();
+            if(tieneResultados(rs)){
+                return rs;
+            }else{
+                return null;
+            }
+        }catch(SQLException ex){
+            return null;
+        }
+    }
 }
 
     
