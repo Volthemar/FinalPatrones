c9b4e3cb7a57a7d4ef33d948fcd731e3aa880f0d
David Arias <davidariascalderon04@gmail.com>
2024-04-28 11:50:11 -0500

    prueba spring

--- Backend/Springboot/Hola/src/main/java/com/example/Hola/models/UserModel.java
+++ Backend/Springboot/Hola/src/main/java/com/example/Hola/models/UserModel.java
@@ -1,12 +1,21 @@
 package com.example.Hola.models;
 
+import java.util.Date;
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuario")
 public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     @Column
     private String nombre;
 
@@ -19,18 +28,49 @@ public class UserModel {
     @Column
     private String usuario;
 
+    @Column
+    private String contrasena;
+
+    @Column
+    private boolean estado;
+
+    @Column
+    private Date fecha_creacion;
+
+    
 
 
+    /**
+     * @return long return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     * @return String return the identificacion
+     */
     public String getIdentificacion() {
         return identificacion;
     }
@@ -42,20 +82,74 @@ public class UserModel {
         this.identificacion = identificacion;
     }
 
+    /**
+     * @return String return the correo
+     */
     public String getCorreo() {
         return correo;
     }
 
+    /**
+     * @param correo the correo to set
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
+    /**
+     * @return String return the usuario
+     */
     public String getUsuario() {
         return usuario;
     }
 
+    /**
+     * @param usuario the usuario to set
+     */
     public void setUsuario(String usuario) {
         this.usuario = usuario;
+    }
+
+    /**
+     * @return String return the contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * @param contrasena the contrasena to set
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    /**
+     * @return boolean return the estado
+     */
+    public boolean isEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return Date return the fecha_creacion
+     */
+    public Date getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    /**
+     * @param fecha_creacion the fecha_creacion to set
+     */
+    public void setFecha_creacion(Date fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
     }
 
 }
