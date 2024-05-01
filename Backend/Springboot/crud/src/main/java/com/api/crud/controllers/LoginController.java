a134e69f554c667aeea18674b531131ac8b2c152
David Arias <davidariascalderon04@gmail.com>
2024-04-30 21:29:49 -0500

    envio de correo exitoso e integracion con el login, hace falta probar en el login

--- Backend/Springboot/crud/src/main/java/com/api/crud/controllers/LoginController.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/controllers/LoginController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 import com.api.crud.models.UsuarioModel;
 import com.api.crud.DTO.LoginRequest;
+import com.api.crud.services.IEmailService;
 import com.api.crud.services.UsuarioService;
+import com.api.crud.services.models.EmailDTO;
+
+import jakarta.mail.MessagingException;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.Map;
-
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
-import java.util.Properties;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/login")
@@ -23,17 +23,27 @@ public class LoginController {
     @Autowired
     private UsuarioService userService;
 
+    @Autowired
+    private IEmailService emailService;
+
     @PostMapping("")
-    public Map<String,Object> login(@RequestBody LoginRequest loginRequest) {
+    public Map<String,Object> login(@RequestBody LoginRequest loginRequest) throws MessagingException{
         String usuario = loginRequest.getUsuario();
         String contrasena = loginRequest.getContrasena();
+        EmailDTO email = new EmailDTO();
         Optional<UsuarioModel> usuarioLoggeado = this.userService.login(usuario,contrasena);
         if (!usuarioLoggeado.isEmpty()){
+            
             if (usuarioLoggeado.get().isEstado()) {
                 usuarioLoggeado.get().setNum_intentos(0);
                 userService.guardarUsuario(usuarioLoggeado.get());
+                email.setDestinatario(usuarioLoggeado.get().getCorreo());
+                email.setMensaje("123456"); // AQUI VA EL CODIGO ALEATORIO
+                email.setAsunto("Código de verificación");
+                emailService.enviarCorreo(email);
                 return Map.of("data",this.userService.login(usuario,contrasena),"msg","Usuario habilitado");
             }else{
+                
                 return Map.of("data", "", "msg", "Usuario bloqueado");
             }
         }else{
@@ -46,6 +56,10 @@ public class LoginController {
                 if (intentos >=3){
                     usuarioExiste.get().setEstado(false);
                     userService.guardarUsuario(usuarioExiste.get());
+                    email.setDestinatario(usuarioLoggeado.get().getCorreo());
+                    email.setMensaje("Su cuenta ha sido bloqueada, por favor comuniquese con administración");
+                    email.setAsunto("Bloqueo de cuenta");
+                    emailService.enviarCorreo(email);
                     return Map.of("data", "", "msg", "El usuario ha sido bloqueado por exceso de intentos");
                 }else{
                     return Map.of("data", "", "msg", "Contraseña incorrecta");
@@ -56,35 +70,5 @@ public class LoginController {
             }
         }
     }
-    // private String generateVerificationCode() {
-    //     // Generar un código de verificación aleatorio
-    //     return UUID.randomUUID().toString();
-    // }
 
-    // private void sendVerificationEmail(String usuario, String verificationCode) {
-    //     // Configurar la conexión a la cuenta de correo electrónico
-    //     Properties props = new Properties();
-    //     props.put("mail.smtp.host", "your-smtp-host");
-    //     props.put("mail.smtp.port", "your-smtp-port");
-    //     props.put("mail.smtp.auth", "true");
-
-    //     // Enviar el correo electrónico
-    //     Session session = Session.getInstance(props, new Authenticator() {
-    //         protected PasswordAuthentication getPasswordAuthentication() {
-    //             return new PasswordAuthentication("your-email", "your-password");
-    //         }
-    //     });
-
-    //     try {
-    //         Message message = new MimeMessage(session);
-    //         message.setFrom(new InternetAddress("your-email"));
-    //         message.setRecipient(Message.RecipientType.TO, new InternetAddress(usuario));
-    //         message.setSubject("Verificación de cuenta");
-    //         message.setText("Por favor, ingrese el siguiente código de verificación: " + verificationCode);
-
-    //         Transport.send(message);
-    //     } catch (MessagingException e) {
-    //         // Manejar la excepción
-    //     }
-    // }
 }
--- Backend/Springboot/crud/src/main/java/com/api/crud/controllers/UsuarioController.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/controllers/UsuarioController.java
@@ -2,15 +2,21 @@ package com.api.crud.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.crud.services.IEmailService;
 import com.api.crud.services.UsuarioService;
+import com.api.crud.services.models.EmailDTO;
+
+import jakarta.mail.MessagingException;
+
 import com.api.crud.models.UsuarioModel;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +28,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UsuarioController {
     @Autowired
     private UsuarioService userService;
+    @Autowired
+    IEmailService emailService;
 
     @GetMapping("/getUsuarios")
-    public ArrayList<UsuarioModel> getUsuarios() {
-        return this.userService.getUser();
+    public ResponseEntity<String> getUsuarios() throws MessagingException{
+        EmailDTO email = new EmailDTO();
+        email.setMensaje("123456");
+        email.setAsunto("prueba 1");
+        email.setDestinatario("davariasc@udistrital.edu.co");
+        emailService.enviarCorreo(email);
+        return new ResponseEntity<>("Correo enviado exitosamente",HttpStatus.OK);
     }
 
     @PostMapping("/postUsuario")
