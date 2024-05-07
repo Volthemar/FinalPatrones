508f8e3531c024f0ff5b76f657e81ae1ade92506
Adrianシロ <adrianstratos@gmail.com>
2024-05-06 19:17:53 -0500

    Vista gerencial

---
+++ Frontend/app/src/Components/SidebarButton/SidebarButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SidebarButton.css'
+
+function SidebarButton({URL_BTN, URL_IMG, nombre}) {
+
+  const handleClick = () => {
+    window.location.href = URL_BTN;
+  };
+
+  return (
+    <div className="sidebarButton" onClick={handleClick}>
+      <img src={URL_IMG} alt="" />
+      <p>{nombre}</p>
+    </div>
+  );
+}
+
+export default SidebarButton;
---
+++ Frontend/app/src/Views/Gerente/Gerente.jsx
@@ -0,0 +1,27 @@
+import Sidebar from '../../Components/Sidebar/Sidebar.jsx'
+import './Gerente.css'
+
+function Gerente() {
+    return (
+        <>
+            <Sidebar></Sidebar>
+            <div id="contenidoGerente">
+            {/*<Router>
+                <Routes>
+                    <Route path="/Impacto" element={A} />
+                    <Route path="/Ubicaciones" element={B} />
+                    <Route path="/Cuentas" element={C} />
+                    <Route path="/Trazabilidad" element={D} />
+                    <Route path="/Modificaciones" element={E} />
+                    <Route path="/Rentabilidad" element={F} />
+                </Routes>
+            </Router>*/}
+                <h2>Estadísticas de uso</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quae adipisci. Ea quaerat 
+                    quidem unde placeat sequi, rem quae, ipsum illo enim nulla ad suscipit esse, earum amet minus 
+                    veritatis!</p>
+            </div>
+        </>
+    );
+}
+export default Gerente;
