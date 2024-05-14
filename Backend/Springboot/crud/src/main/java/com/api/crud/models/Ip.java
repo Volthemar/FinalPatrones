a23e5557602214cc007a7533ffe41f5a6052e575
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-14 05:25:45 -0500

    Create Ip.java
    Adición del modelo de IP con setters y getters

---
+++ Backend/Springboot/crud/src/main/java/com/api/crud/models/Ip.java
@@ -0,0 +1,39 @@
+public class Ip {
+    private int id;
+    private String direccionIp;
+    private Integer usuarioFk;
+    private Date fechaCreacion;
+
+    // Getters and setters for each field
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDireccionIp() {
+        return direccionIp;
+    }
+
+    public void setDireccionIp(String direccionIp) {
+        this.direccionIp = direccionIp;
+    }
+
+    public Integer getUsuarioFk() {
+        return usuarioFk;
+    }
+
+    public void setUsuarioFk(Integer usuarioFk) {
+        this.usuarioFk = usuarioFk;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+}
