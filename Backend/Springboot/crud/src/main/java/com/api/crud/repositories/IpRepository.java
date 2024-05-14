f7df0af8e5cd5a32e8f75dac0bee845de5c16509
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-14 11:33:22 -0500

    Update IpRepository.java

--- Backend/Springboot/crud/src/main/java/com/api/crud/repositories/IpRepository.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/repositories/IpRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IpRepository extends JpaRepository<Ip, Long> {
+    List<Ip> findByUsuarioFk(Long usuarioFk);
+    List<Ip> findByDireccionIp(String direccionIp);
 }
