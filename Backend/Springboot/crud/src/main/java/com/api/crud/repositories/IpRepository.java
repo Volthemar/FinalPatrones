c8975c8973b7b1b047e44766750629e9872b1ddd
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-14 11:31:51 -0500

    Update IpRepository.java

--- Backend/Springboot/crud/src/main/java/com/api/crud/repositories/IpRepository.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/repositories/IpRepository.java
@@ -1,1 +1,9 @@
+package com.api.crud.repositories;
 
+import com.api.crud.models.Ip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IpRepository extends JpaRepository<Ip, Long> {
+}
