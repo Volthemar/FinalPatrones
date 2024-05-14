a8262525a476b6cdaeb29e8d5006041655e50a36
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-14 11:11:47 -0500

    Update IpService.java

--- Backend/Springboot/crud/src/main/java/com/api/crud/services/IpService.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/services/IpService.java
@@ -1,1 +1,26 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+@Service
+public class IpService {
+
+    @Autowired
+    private DataBase dataBase;
+
+    public void saveIpAddress(String ipAddress, Integer userId) {
+        Ip ip = new Ip();
+        ip.setDireccionIp(ipAddress);
+        ip.setUsuarioFk(userId);
+        ip.setFechaCreacion(new Date());
+
+        try {
+            dataBase.saveIpAddress(ip);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
