9cdcd89b486bc5104316e35414f3ad8e18e907f3
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-18 20:53:14 -0500

    Create CancelReservationRequest.java

---
+++ Backend/Springboot/crud/src/main/java/com/api/crud/DTO/CancelReservationRequest.java
@@ -0,0 +1,14 @@
+package com.api.crud.dto;
+
+public class CancelReservationRequest {
+    private Long cupoId;
+
+    // Getters and Setters
+    public Long getCupoId() {
+        return cupoId;
+    }
+
+    public void setCupoId(Long cupoId) {
+        this.cupoId = cupoId;
+    }
+}
