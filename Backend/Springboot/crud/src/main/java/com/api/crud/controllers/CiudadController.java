b6807ce86d9b8a9e0de60f48756503e3a44dc0b7
David Arias <davidariascalderon04@gmail.com>
2024-05-19 12:40:29 -0500

    controller para obtener ciudades

---
+++ Backend/Springboot/crud/src/main/java/com/api/crud/controllers/CiudadController.java
@@ -0,0 +1,25 @@
+package com.api.crud.controllers;
+
+import java.util.Map;
+import java.util.Vector;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.crud.services.CiudadService;
+
+@RestController
+@RequestMapping("")
+public class CiudadController {
+    @Autowired
+    private CiudadService ciudadService;
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/obenerCiudades")
+    public Map<String,Object> obenerCiudades(){
+        return Map.of("data", ciudadService.obtenerCiudades(), "msg", "Ciudades");
+    }
+}
