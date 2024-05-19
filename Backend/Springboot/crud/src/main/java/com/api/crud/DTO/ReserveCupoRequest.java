ac0221f7ffceba4b6c19ebf0b3682ff657efe412
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-18 20:52:29 -0500

    Create ReserveCupoRequest.java

---
+++ Backend/Springboot/crud/src/main/java/com/api/crud/DTO/ReserveCupoRequest.java
@@ -0,0 +1,14 @@
+package com.api.crud.dto;
+
+public class ReserveCupoRequest {
+    private Long userId;
+
+    // Getters and Setters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
