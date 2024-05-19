71cb15f5a48da6791b487bc2d4826c989936a9de
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-18 20:50:40 -0500

    Update NavigationController.java

--- Backend/Springboot/crud/src/main/java/com/api/crud/controllers/NavigationController.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/controllers/NavigationController.java
@@ -1,29 +1,25 @@
 package com.api.crud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.api.crud.dto.IpCaptureRequest;
 import com.api.crud.services.IpService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 @RestController
-@RequestMapping("/navigation")
+@RequestMapping("/api/navigation")
 public class NavigationController {
 
     @Autowired
     private IpService ipService;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/track")
-    public Map<String, Object> trackNavigation(HttpServletRequest request) {
-        String ipAddress = request.getRemoteAddr();
-        ipService.saveIpAddress(ipAddress, null); // Save IP without a user ID since this is a visitor
-
-        return Map.of("data", "IP logged", "ipAddress", ipAddress);
+    public void trackVisitor(HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        IpCaptureRequest ipCaptureRequest = new IpCaptureRequest();
+        ipCaptureRequest.setIpAddress(clientIp);
+        ipService.captureIp(ipCaptureRequest);
     }
 }
