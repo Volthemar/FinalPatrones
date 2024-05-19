4c21a555cbbea75a503455ab5ee59c67b69d16ed
Volthemar <54372249+Volthemar@users.noreply.github.com>
2024-05-18 20:52:45 -0500

    Create OccupyCupoRequest.java

---
+++ Backend/Springboot/crud/src/main/java/com/api/crud/DTO/OccupyCupoRequest.java
@@ -0,0 +1,14 @@
+package com.api.crud.dto;
+
+public class OccupyCupoRequest {
+    private Long cupoId;
+
+    // Getters and Setters
+    public Long getCupoId() {
+        return cupoId;
+    }
+
+    public void setCupoId(Long cupoId) {
+        this.cupoId = cupoId;
+    }
+}
